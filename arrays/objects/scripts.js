cf989171a865732729d97ffac1662f8c7c04f341
ohkhaila <khaila.jones@lessannoyingcrm.com>
2020-07-01 10:58:06 -0500

    pre lesson code

---
+++ arrays/objects/scripts.js
@@ -0,0 +1,21 @@
+//title
+//body
+//views
+//comments
+    //author
+    //body
+//isLive - can be true or false
+
+
+let blogPost = {
+    title: "a",
+    body: "b",
+    author: "c",
+    views: 10,
+    comments: [
+        { author: 'a', body: 'b'},
+        { author: 'c', body: 'd'}
+    ],
+    isLive: true
+}
+console.log(blogPost);
---
+++ arrays/scripts.js
@@ -0,0 +1,6 @@
+
+let selectedFood = []; //empty array 
+
+selectedFood[0] = 'avocado';
+
+console.log(selectedFood[0]);
